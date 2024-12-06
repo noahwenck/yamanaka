61fb9a129e28da428a8b0a710d22a051dd05403a
noahwenck <123976574+noahwenck@users.noreply.github.com>
2024-12-05 20:17:04 -0600

    add bar chart func

--- stats.py
+++ stats.py
@@ -9,6 +9,12 @@ from gather import get_user_info
 JPG = ".jpg"
 
 def scatter(username):
+    """
+    Generates a scatter plot of films for a specified user.
+
+    Parameters:
+    username (str): The username to fetch film data for.
+    """
     films = pd.read_json(io.StringIO(get_user_info(username).text))
 
     sns.scatterplot(x="Year", y="Average Rating", hue="Primary Language", data=films)
@@ -16,3 +22,30 @@ def scatter(username):
     plt.savefig(username + "-Year-AverageRating-PrimaryLanguage" + JPG, bbox_inches='tight')
 
     Image.open(username + "-Year-AverageRating-PrimaryLanguage" + JPG).show()
+    plt.clf()
+
+def bar(username, category):
+    """
+    Generates a bar chart for the given category of films for a specified user.
+
+    Parameters:
+    username (str): The username to fetch film data for.
+    category (str): The category to analyze and plot (e.g., 'Director').
+    """
+    films = pd.read_json(io.StringIO(get_user_info(username).text))
+
+    films[category] = films[category].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+    films_exploded = films.explode(category)
+    director_counts = films_exploded[category].value_counts()
+    director_counts = director_counts[director_counts > 1]
+    director_counts.sort_values(ascending=False, inplace=True)
+
+    sns.barplot(x=director_counts.index, y=director_counts.values)
+    plt.xlabel(category)
+    plt.ylabel("Number of Films")
+    plt.xticks(rotation=45, ha='right')
+    plt.legend([],[], frameon=False)
+    plt.savefig(username + "-" + category + JPG, bbox_inches='tight')
+
+    Image.open(username + "-" + category + JPG).show()
+    plt.clf()
--- yamanaka.py
+++ yamanaka.py
@@ -5,13 +5,17 @@ sys.dont_write_bytecode = True
 import argparse
 
 from stats import scatter
+from stats import bar
 
 parser = argparse.ArgumentParser(description="Whats up", formatter_class=argparse.RawTextHelpFormatter)
-
-# REQUIRED
 #todo: add check for if user actually exists
 parser.add_argument("user", help="Letterboxd username")
-
 args = parser.parse_args()
 
 scatter(args.user)
+
+bar(args.user, "Director")
+bar(args.user, "Spoken Language")
+bar(args.user, "Country")
+bar(args.user, "Genre")
+bar(args.user, "Studio")
